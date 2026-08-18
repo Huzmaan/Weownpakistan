6ec4d48ffb6099d4178f832632822aafdb7e9127
Huzmaan Pasta <princehuzmaanpasta@gmail.com>
2026-08-18 18:39:08 +0500

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    base: "/test-project/",
+  },
+
   tanstackStart: {
     server: { entry: "server" },
+  },
+
+  nitro: {
+    preset: "node-server",
   },
 });
